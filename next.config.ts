9502e05c6a895bea20c89ba1e94f5587a7c0c98c
r.yone <103408503+ryou-yonezu@users.noreply.github.com>
2025-05-24 20:52:50 +0900

    不要なデプロイを削除

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  output: 'export', // この行を追加
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/pagePlant/' : undefined,
-  basePath: process.env.NODE_ENV === 'production' ? '/pagePlant' : undefined,
+  // output: 'export', // この行を追加
+  // assetPrefix: process.env.NODE_ENV === 'production' ? '/pagePlant/' : undefined,
+  // basePath: process.env.NODE_ENV === 'production' ? '/pagePlant' : undefined,
   images: {
     unoptimized: true,
   },
